5ebeb0e3a98988dff3363fcdf91575660f167d87
jt5493 <jt5493@hotmail.com>
2020-08-29 19:49:45 -0700

    first get request working

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hi")
+const endPoint = "http://localhost:3000/api/v1/workouts"
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    getWorkouts()
+})
+
+function getWorkouts(){
+    fetch(endPoint)
+    .then(response => response.json())
+    .then(workouts => {
+        workouts.data.forEach(workout => {
+            const workoutData = `
+            <div data-id=${workout.id}>
+                <h2> ${workout.attributes.title}</h2>
+                <h3> ${workout.attributes.location}</h3>
+                <h3> ${workout.attributes.category.name}</h3>
+                <p>${workout.attributes.description}</p>
+                <button data-id=${workout.id}> Edit</button>
+            </div>
+            <br><br>`;
+
+            document.querySelector('#workout-container').innerHTML+= workoutData
+        })
+    })
+}
