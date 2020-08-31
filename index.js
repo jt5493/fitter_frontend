b38d7f81513d799d0f0bfceb263894dc9bf8450c
jt5493 <jt5493@hotmail.com>
2020-08-30 17:54:56 -0700

    created new workout form

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const endPoint = "http://localhost:3000/api/v1/workouts"
 
 document.addEventListener('DOMContentLoaded', () => {
     getWorkouts()
+
+    const createWorkoutForm = document.querySelector("#create-workout-form")
+
+    createWorkoutForm.addEventListener("submit", (e) => createFormHandler(e))
 })
 
 function getWorkouts(){
@@ -27,4 +31,18 @@ function getWorkouts(){
             document.querySelector('#workout-container').innerHTML+= workoutData
         })
     })
+}
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const locationInput = document.querySelector('#input-location').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+
+    postFetch(titleInput, descriptionInput, locationInput, categoryId)
+}
+
+function postFetch(title, description, location, category_id) {
+    
 }
